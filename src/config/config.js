d36d9bd11377ca5a5932e4cb3e1eb00abe0d61ff
lucasplr <lucasriffel55@gmail.com>
2022-10-19 21:16:27 -0300

    env data

---
+++ src/config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config
+
+module.exports = {
+    development: {
+      username: process.env.DB_USER,
+      password: process.env.password,
+      database: process.env.database,
+      host: process.env.host,
+      dialect: "postgres"
+    },
+    test: {
+      username: process.env.username,
+      password: process.env.password,
+      database: process.env.database,
+      host: process.env.host,
+      dialect: "postgres"
+    },
+    production: {
+      username: process.env.username,
+      password: process.env.password,
+      database: process.env.database,
+      host: process.env.host,
+      dialect: "postgres"
+    }
+  }
