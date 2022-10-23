52c8afccff732cddebbc18037e9c491a979b856b
lucasplr <lucasriffel55@gmail.com>
2022-10-22 23:51:35 -0300

    refresh token

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 module.exports = {
     development: {
       username: process.env.DB_USER,
-      password: process.env.password,
+      password: process.env.DB_PASSWORD,
       database: process.env.database,
       host: process.env.host,
       dialect: "postgres"
---
+++ src/services/generateRefresgToken.js
@@ -0,0 +1,42 @@
+const jwt = require('jsonwebtoken')
+
+const dayjs = require('dayjs')
+
+const database = require('../models')
+
+module.exports = {
+
+    async generateRefreshToken(userId){
+
+        const payload = {
+            userId: userId
+        }
+
+        const refresh_token = await jwt.sign(payload, process.env.secret_refresh_token, {
+            expiresIn: process.env.expires_in_refresh_token
+        })
+
+
+        const refresh_token_expires_date = dayjs().add(process.env.expires_in_refresh_token_days, 'days').toDate()
+        console.log(refresh_token_expires_date)
+
+        try{
+
+            await database.userToken.destroy({
+                where: {
+                    userId: userId
+                }
+            })
+
+            const refreshToken = await database.userToken.create({
+                userId: userId,
+                refreshToken: refresh_token,
+                expiresDate: refresh_token_expires_date
+            })
+
+            return refreshToken
+        }catch(err){
+            return err.message
+        }
+    }
+}
